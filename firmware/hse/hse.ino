2837b16ca51540a518bc41cab11f717d01b8ecfc
Nick Pascucci <npascut1@gmail.com>
2018-11-07 22:24:22 -0500

    Fix frequency mode

--- firmware/hse/hse.ino
+++ firmware/hse/hse.ino
@@ -4,49 +4,81 @@
 #define PACKET_SIZE 8
 
 #define DEFAULT_INTENSITY 128
-#define ON 1
-#define OFF 0
 
+/**
+ * In intensity mode, the front and back motors are driven using a 
+ * linear relationship between the PWM rate and the value given on
+ * the serial port. The value 0 is "off", and 255 is the highest
+ * intensity vibration.
+ */
 #define INTENSITY 1
+
+/**
+ * In frequency mode the values for the motors control the duration
+ * between pulses at maximum intensity, with 0 being the longest 
+ * duration and 255 the shortest.
+ */
 #define FREQUENCY 2
+#define FREQ_MODE_PULSE_WIDTH 50
 
 int mode = INTENSITY;
-int front_state = OFF;
+int front_state = LOW;
 long last_front_state_change = 0;
-int back_state = OFF;
+int back_state = LOW;
 long last_back_state_change = 0;
 
-int front_intensity = 0;
-int back_intensity = 0;
+int front_value = 0;
+int back_value = 0;
 
-char front_buf[10];
-char back_buf[10];
+char front_buf[PACKET_SIZE + 1];
+char back_buf[PACKET_SIZE + 1];
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(MOTOR_F, OUTPUT);
   pinMode(MOTOR_B, OUTPUT);
   Serial.println("I'm alive!");
 }
 
 void loop() {
-  // Look for intensity commands, which look like {FFF;BBB}
-  // (e.g. {124;256} to set the front to intensity 124 and 
-  //  the back to intensity 256)
+  update_motors();
+  read_values();
+}
+
+void read_values() {
+  // Look for value commands, which look like {FFF;BBB}
+  // (e.g. {124;256} to set the front to value 124 and 
+  //  the back to value 256)
   while (Serial.available() > 0) {
     int in_byte = Serial.read();
-    if (in_byte == '{') { // Opening brace; begin parsing commands
+
+     if (in_byte == 'I') {
+      mode = INTENSITY;
+      Serial.print("Mode: Intensity");
+    } else if (in_byte == 'F') {
+      mode = FREQUENCY;
+      last_front_state_change = millis();
+      last_back_state_change = millis();
+      front_state = LOW;
+      back_state = LOW;
+      Serial.print("Mode: Frequency");
+    } else if (in_byte == '{') {
       Serial.println("Reading packet");
-      memset(front_buf, 0, sizeof(front_buf)); // Initialize read buffers
+
+      // Initialize read buffers to 0
+      memset(front_buf, 0, sizeof(front_buf));
       memset(back_buf, 0, sizeof(back_buf));
       int success = 0;
 
+      // Parse the first part of the packet
       for (int i = 0; i < PACKET_SIZE; i++) {
         while (!Serial.available()) {
-          // Wait
+          // Wait for data to become available
         }
+
         in_byte = Serial.read();
         Serial.print((char) in_byte);
+
         if (in_byte == ';') {
           Serial.println(" <- Saw separator, moving on");
           break;
@@ -57,10 +89,11 @@ void loop() {
       
       for (int i = 0; i < PACKET_SIZE; i++) {
         while (!Serial.available()) {
-          // Wait
+          // Wait for data to become available
         }
         in_byte = Serial.read();
         Serial.print((char) in_byte);
+
         if (in_byte == '}') { // Yay! A full packet arrived.
           success = 1;
           Serial.println(" <- Received full packet");
@@ -72,54 +105,49 @@ void loop() {
       
       if (success) {
         Serial.println("Here's what I read:");
-        Serial.println(front_buf);
-        Serial.println(back_buf);
-        front_intensity = atoi(front_buf);
-        back_intensity = atoi(back_buf);
-        Serial.print(front_intensity);
-        Serial.print(",");
-        Serial.println(back_intensity);
+        Serial.print("{");
+        Serial.print(front_buf);
+        Serial.print(";");
+        Serial.print(back_buf);
+        Serial.println("}");
+
+        front_value = atoi(front_buf);
+        back_value = atoi(back_buf);
       } else {
         Serial.print("Failed to read packet");
         continue;
       }
-    } else if (in_byte == 'I') {
-      mode = INTENSITY;
-      Serial.print("Mode: Intensity");
-    } else if (in_byte == 'F') {
-      mode = FREQUENCY;
-      last_front_state_change = millis();
-      last_back_state_change = millis();
-      front_state = OFF;
-      back_state = OFF;
-      Serial.print("Mode: Frequency");
     }
   }
+}
 
-  if (mode == INTENSITY) {
-    analogWrite(MOTOR_F, front_intensity);
-    analogWrite(MOTOR_B, back_intensity);
-  } else { 
-    if (front_state == ON) {
-      analogWrite(MOTOR_F, DEFAULT_INTENSITY);
-    } else {
-      analogWrite(MOTOR_F, 0);
-    }
-
-    if (back_state == ON) {
-      analogWrite(MOTOR_B, DEFAULT_INTENSITY);
-    } else {
-      analogWrite(MOTOR_F, 0);
-    }
-
-    if (last_front_state_change + front_intensity < millis()) {
+void update_motors() {
+   if (mode == INTENSITY) {
+    analogWrite(MOTOR_F, front_value);
+    analogWrite(MOTOR_B, back_value);
+  } else {
+    if (last_front_state_change + delay_from_value(front_state, front_value) < millis()) {
       front_state = !front_state;
+      digitalWrite(MOTOR_F, front_state);
       last_front_state_change = millis();
     }
 
-    if (last_back_state_change + back_intensity < millis()) {
+    if (last_back_state_change + delay_from_value(back_state, back_value) < millis()) {
       back_state = !back_state;
+      digitalWrite(MOTOR_B, back_state);
       last_back_state_change = millis();
     }
+  }
+}
+
+/**
+ * Get the delay in milliseconds that the frequency mode should 
+ * wait for a given value.
+ */
+int delay_from_value(int state, int value) {
+  if (state == HIGH) {
+    return FREQ_MODE_PULSE_WIDTH;
+  } else {
+    return (255 - value) * 4;
   }
 }
